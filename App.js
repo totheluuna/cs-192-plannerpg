db31e340d471072a1d56887c6a56d5850f10c8fc
lunaouji <dgdilangalen@up.edu.ph>
2019-02-07 16:20:29 +0800

    Fixed custom font error

--- App.js
+++ App.js
@@ -1,94 +1,35 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Card, CardItem, Text, Body, Left, Right, Title,
-Button, Icon, Fab, View } from 'native-base';
-export default class CardHeaderFooterExample extends Component {
+import { View } from 'react-native';
+import { Container } from 'native-base';
+import { Font, AppLoading } from 'expo';
+import ViewCorkboard from './app/components/ViewCorkboard';
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isReady: false
+    };
+  }
+  componentWillMount() {
+    this.loadFonts();
+  }
+  async loadFonts() {
+    await Expo.Font.loadAsync({
+      Roboto: require("./node_modules/native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("./node_modules/native-base/Fonts/Roboto_medium.ttf"),
+    });
+    this.setState({ isReady: true });
+  }
 
   render() {
+    if (!this.state.isReady) {
+      return <Expo.AppLoading />;
+    }
+
     return (
       <Container>
-				<Header>
-					<Left>
-						<Button transparent>
-							<Icon name='arrow-back' />
-						</Button>
-					</Left>
-					<Body>
-						<Title>Corkboard</Title>
-					</Body>
-					<Right>
-						<Button transparent>
-							<Icon name='add' />
-						</Button>
-						<Button transparent>
-							<Icon name='trash' />
-						</Button>
-						<Button transparent>
-							<Icon name='more' />
-						</Button>
-					</Right>
-				</Header>
-
-
-        <Content>
-          <Card>
-            <CardItem header>
-              <Text>CS 199</Text>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Text>
-                  This is a test Memo that I want to reflect on the app
-                </Text>
-              </Body>
-            </CardItem>
-            <CardItem footer>
-              <Text>December 30, 2019</Text>
-            </CardItem>
-         </Card>
-
-		 <Card>
-		   <CardItem header>
-			 <Text>THINGS TO BUY</Text>
-		   </CardItem>
-		   <CardItem>
-			 <Body>
-			   <Text>
-				 This is a test Memo that I want to reflect on the app
-				 This is a test Memo that I want to reflect on the app
-				 This is a test Memo that I want to reflect on the app
-				 This is a test Memo that I want to reflect on the app
-				 This is a test Memo that I want to reflect on the app
-				 This is a test Memo that I want to reflect on the app
-				 This is a test Memo that I want to reflect on the app
-				 This is a test Memo that I want to reflect on the app
-				 This is a test Memo that I want to reflect on the app
-			   </Text>
-			 </Body>
-		   </CardItem>
-		   <CardItem footer>
-			 <Text>December 30, 2019</Text>
-		   </CardItem>
-		</Card>
-
-		<Card>
-		  <CardItem header>
-			<Text>THINGS TO BUY</Text>
-		  </CardItem>
-		  <CardItem>
-			<Body>
-			  <Text>
-				This is a test Memo that I want to reflect on the app
-			  </Text>
-			</Body>
-		  </CardItem>
-		  <CardItem footer>
-			<Text>December 30, 2019</Text>
-		  </CardItem>
-	   </Card>
-
-
-
-        </Content>
+        <ViewCorkboard/>
       </Container>
     );
   }
